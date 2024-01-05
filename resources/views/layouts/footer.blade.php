fa46dad29274837838da30c11dc2a555ae2e9528
R3tarb0y <muhammadrezaputra.anisi@gmail.com>
2024-01-05 19:42:10 +0700

    First Commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+
+    </div>
+        <span>Copyright PT Pertamina Lubricants &copy;<?php echo date("Y") ?></span>
+      </div>
+    </div>
+  </footer>
